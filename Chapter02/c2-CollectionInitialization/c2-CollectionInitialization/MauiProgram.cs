0bef2db085aca343e57a6125f27f7aba0521496a
Conti sig Massimo <mconti@ceia-spa.com>
2025-05-05 09:49:49 +0200

    Refactored. It needs to fix the warnings.

--- Chapter02/c2-CollectionInitialization/c2-CollectionInitialization/MauiProgram.cs
+++ Chapter02/c2-CollectionInitialization/c2-CollectionInitialization/MauiProgram.cs
@@ -1,24 +1,26 @@
 ﻿using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
 
-namespace c2_DecoupleViewAndViewModel {
-    public static class MauiProgram {
-        public static MauiApp CreateMauiApp() {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .UseMauiCommunityToolkit()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+namespace c2_DecoupleViewAndViewModel;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }
---
+++ Chapter02/c2-CollectionInitialization/c2-CollectionInitialization/ViewModels/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace c2_DecoupleViewAndViewModel.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    ObservableCollection<Customer>? customers;
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(InitializeCommand))]
+    bool isInitialized;
+
+    [RelayCommand(CanExecute = nameof(CanInitialize))]
+    async Task InitializeAsync()
+    {
+        Customers = [.. await DummyService.GetCustomersAsync()];
+        IsInitialized = true;
+    }
+    bool CanInitialize() => !IsInitialized;
+
+}
+public class Customer
+{
+    public int ID
+    {
+        get;
+        set;
+    }
+    public string? Name
+    {
+        get;
+        set;
+    }
+}
+
+public static class DummyService
+{
+    public static async Task<IEnumerable<Customer>> GetCustomersAsync()
+    {
+        await Task.Delay(5000);
+        return [new Customer(){ ID = 1, Name = "Jim" }, new Customer(){ ID = 2, Name = "Bob" }];
+    }
+}
