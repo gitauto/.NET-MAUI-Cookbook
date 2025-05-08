05d217e153d133cc1742c106ebd6625358b49e7e
Conti sig Massimo <mconti@ceia-spa.com>
2025-05-08 08:20:55 +0200

    .

--- Chapter02/c2-CollectionInitialization/c2-CollectionInitialization/ViewModels/MainViewModel.cs
+++ Chapter02/c2-CollectionInitialization/c2-CollectionInitialization/ViewModels/MainViewModel.cs
@@ -9,11 +9,11 @@ namespace c2_DecoupleViewAndViewModel.ViewModels;
 public partial class MainViewModel : ObservableObject
 {
     [ObservableProperty]
-    public partial ObservableCollection<Customer>? Customers { get; set; }
+    public partial ObservableCollection<Customer>? Customers { get; set; }      // <LangVersion>preview</LangVersion> in the project file is required for this to work
 
     [ObservableProperty]
     [NotifyCanExecuteChangedFor(nameof(InitializeCommand))]
-    public partial bool IsInitialized { get; set; }
+    public partial bool IsInitialized { get; set; }                             // <LangVersion>preview</LangVersion> in the project file is required for this to work
 
     [RelayCommand(CanExecute = nameof(CanInitialize))]
     private async Task InitializeAsync()
